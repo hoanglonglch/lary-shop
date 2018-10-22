789f19c1ccba4a635d736c2a49f23f3e351a39ae
hoanglonglch <hoanglonglch@gmail.com>
2018-10-22 21:55:33 +0700

    Using async pipe with Observable object

--- src/app/bs-navbar/bs-navbar.component.ts
+++ src/app/bs-navbar/bs-navbar.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {AngularFireAuth} from 'angularfire2/auth';
 import * as firebase from 'firebase';
+import {Observable} from 'rxjs';
 
 
 @Component({
@@ -10,18 +11,19 @@ import * as firebase from 'firebase';
 })
 export class BsNavbarComponent implements OnInit {
 
-  user: firebase.User;
+  user$: Observable<firebase.User>;
 
   constructor(public afAuth: AngularFireAuth) {
 
   }
 
   ngOnInit() {
-    this.afAuth.authState.subscribe( dataFromGoogle => {
+    this.user$ = this.afAuth.authState;
+    /*this.afAuth.authState.subscribe( dataFromGoogle => {
       this.user = dataFromGoogle;
       let displayName = this.user ? this.user.displayName : '';
       console.log('[BsNavbarComponent][ngOnit()][GoogleData]', displayName);
-    });
+    });*/
   }
 
   logout(){
