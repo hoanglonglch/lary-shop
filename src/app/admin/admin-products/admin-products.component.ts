f14a8d479d978712b288c394e48985cab1a37a38
Larry <hoanglonglch@gmail.com>
2018-11-09 11:19:44 +0700

    [Product-Management][List-Product] Implement Search feature

--- src/app/admin/admin-products/admin-products.component.ts
+++ src/app/admin/admin-products/admin-products.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {ProductService} from '../../service/product.service';
-import {Observable} from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 import {map, switchMap} from 'rxjs/operators';
+import {Product} from '../../../models/product';
 
 
 @Component({
@@ -9,23 +10,39 @@ import {map, switchMap} from 'rxjs/operators';
   templateUrl: './admin-products.component.html',
   styleUrls: ['./admin-products.component.css']
 })
-export class AdminProductsComponent implements OnInit {
-  products$: Observable<any[]>;
+export class AdminProductsComponent implements OnInit, OnDestroy {
+  products: Product [];
+  filteredProduct: Product [];
+  subscription: Subscription
 
   constructor(private productService: ProductService) {
-    this.products$ = productService.getAll()
+  }
+
+  ngOnInit() {
+    this.subscription = this.productService.getAll()
       .snapshotChanges()
       .pipe(
         map(actions =>
           actions.map(a => {
-            return ({ key: a.key, ...a.payload.val() });
+            return ({key: a.key, ...a.payload.val()});
           })
         )
-      );
+      )
+      .subscribe((products) => {
+        this.filteredProduct = this.products =  products as Product[];
+      });
+
   }
 
-  ngOnInit() {
-
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 
+
+  searchTitle(query: string) {
+  //  Filter the product
+    this.filteredProduct = (query) ? this.products.filter(product => {
+      return (product.title.toLowerCase()).includes(query.toLowerCase());
+    }) : this.products;
+  }
 }
