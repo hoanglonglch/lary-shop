90e87d8517b46fe24c01e6fb7416d5202eebfd51
Larry <hoanglonglch@gmail.com>
2018-11-12 14:42:33 +0700

    [Product-Catalog][Categories] Get List Categories

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {CategoryService} from '../service/category.service';
+import {Observable} from 'rxjs';
+import {map} from 'rxjs/operators';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +11,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  categories$: Observable<any []>;
+
+  constructor(private categoryService: CategoryService,
+              private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.categories$ = this.categoryService.getListCategories()
+      .snapshotChanges()
+      .pipe(
+        map(actions =>
+          actions.map(a => {
+            return ({ key: a.key, ...a.payload.val() });
+          })
+        ));
   }
 
 }
