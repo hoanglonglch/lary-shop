c0b6d0faf73a350332c56e6719bc7c45a57701b8
Larry <hoanglonglch@gmail.com>
2018-11-13 13:52:09 +0700

    [Product-Catalog][Categories] Get List Categories in ProductsComponent

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Observable} from 'rxjs';
 import {ProductService} from '../service/product.service';
+import {CategoryService} from '../service/category.service';
 
 @Component({
   selector: 'app-products',
@@ -10,11 +11,14 @@ import {ProductService} from '../service/product.service';
 export class ProductsComponent implements OnInit {
 
   products$: Observable<any []>;
+  categories$: Observable<any []>;
 
-  constructor(private productService: ProductService) { }
+  constructor(private productService: ProductService,
+              private categoryService: CategoryService) { }
 
   ngOnInit() {
     this.products$ = this.productService.getAll().valueChanges();
+    this.categories$ = this.categoryService.getListCategories().valueChanges();
   }
 
 }
