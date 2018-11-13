723ff6081e740f6c940beb9e7eb7f4d313ee8430
Larry <hoanglonglch@gmail.com>
2018-11-13 13:28:24 +0700

    [Product-Catalog][Products] Get List Products in ProductsComponent

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {Observable} from 'rxjs';
+import {ProductService} from '../service/product.service';
 
 @Component({
   selector: 'app-products',
@@ -7,9 +9,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductsComponent implements OnInit {
 
-  constructor() { }
+  products$: Observable<any []>;
+
+  constructor(private productService: ProductService) { }
 
   ngOnInit() {
+    this.products$ = this.productService.getAll().valueChanges();
   }
 
 }
