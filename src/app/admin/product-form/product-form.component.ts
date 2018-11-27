dd5d75064c7f31862324e1ef5e5e65dc543edec9
Larry <hoanglonglch@gmail.com>
2018-11-27 10:44:57 +0700

    [Product-Management][Form-Edit] Refactor code

--- src/app/admin/product-form/product-form.component.ts
+++ src/app/admin/product-form/product-form.component.ts
@@ -20,7 +20,7 @@ export class ProductFormComponent implements OnInit {
   product$: Observable<any>;
   productForm: FormGroup;
   isSubmit = false;
-  product: Product;
+  product = {} as Product;
   productId: string;
 
   constructor(private categoryService: CategoryService,
@@ -84,7 +84,7 @@ export class ProductFormComponent implements OnInit {
     if (this.productId !== 'new') {
       this.productService.getProduct(this.productId).subscribe( (product: Product) => {
         this.product = product;
-        this.fillTheProductForm(product);
+        // this.fillTheProductForm(product);
       });
     }
   }
@@ -131,11 +131,11 @@ export class ProductFormComponent implements OnInit {
     this.router.navigate(['admin/products']);
   }
 
-  fillTheProductForm(product: Product) {
+  /*fillTheProductForm(product: Product) {
     this.title.setValue(product.title);
     this.price.setValue(product.price);
     this.imageUrl.setValue(product.imageUrl);
     this.category.setValue(product.category);
-  }
+  }*/
 
 }
