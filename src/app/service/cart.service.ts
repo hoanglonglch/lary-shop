ecb5c8e6660f2f7c5f13dc60b6620c1de7ce9626
Larry <hoanglonglch@gmail.com>
2018-11-29 16:08:58 +0700

    [Shopping-Cart][Bug] Memory Leak

--- src/app/service/cart.service.ts
+++ src/app/service/cart.service.ts
@@ -2,6 +2,12 @@ import { Injectable } from '@angular/core';
 import {AngularFireDatabase, AngularFireList} from 'angularfire2/database';
 import {Product} from '../../models/product';
 import {Cart} from '../../models/cart';
+import {map, switchMap} from 'rxjs/operators';
+import {Observable} from 'rxjs';
+import {forEach} from '@angular/router/src/utils/collection';
+import { take } from 'rxjs/operators';
+
+
 
 @Injectable({
   providedIn: 'root'
@@ -9,6 +15,7 @@ import {Cart} from '../../models/cart';
 export class CartService {
 
   cart$: AngularFireList<any []>;
+  subscription;
 
   constructor(private database: AngularFireDatabase) {
     this.cart$ = database.list('/shopping-carts');
@@ -21,15 +28,27 @@ export class CartService {
       productIds: [product.key]
     };
 
-    let cartKey= localStorage.getItem('cartKey');
+    let cartKey = localStorage.getItem('cartKey');
 
-    if(cartKey) {
-      this.database.object('/shopping-cart/' + cartKey).valueChanges().pipe()
-      /*.subscribe((shoppingCart: Cart) => {
-        console.log('shoppingCart', shoppingCart);
-      });*/
+    if (cartKey) {
+      this.database.object('/shopping-carts/' + cartKey).valueChanges()
+      .subscribe((shoppingCart: Cart) => {
+        console.log('cart', shoppingCart);
 
-    }else{
+        let productIds = shoppingCart.productIds;
+
+        if (productIds.indexOf(product.key) > -1) {
+          //  don't update
+
+        } else {
+          productIds.push(product.key);
+          cart.productIds = productIds;
+          this.database.object('/shopping-carts/' + cartKey).update(cart);
+        }
+      });
+
+
+    } else {
       this.database.list('/shopping-carts').push(cart).then(item => {
         console.log('item', item);
         console.log('key', item.key);
