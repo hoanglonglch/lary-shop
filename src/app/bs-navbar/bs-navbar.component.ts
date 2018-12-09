8ac5495650aad0e9e510c7cb3f85e42672b3da21
hoanglonglch <hoanglonglch@gmail.com>
2018-12-09 19:22:30 +0700

    [Shopping-Cart][Quantity] Display Quantity on NavBar

--- src/app/bs-navbar/bs-navbar.component.ts
+++ src/app/bs-navbar/bs-navbar.component.ts
@@ -1,6 +1,9 @@
 import {Component, OnInit} from '@angular/core';
 import {AuthService} from '../service/auth.service';
 import {AppUser} from '../../models/app-user';
+import {CartService} from '../service/cart.service';
+import {ShoppingCart} from '../../models/shopping-cart';
+import {Item} from '../../models/item';
 
 
 @Component({
@@ -11,14 +14,31 @@ import {AppUser} from '../../models/app-user';
 export class BsNavbarComponent implements OnInit {
 
   appUser: AppUser;
-  constructor(public authService: AuthService) {
+  quantity = 0;
+
+  constructor(public authService: AuthService,
+              private cartService: CartService) {
   }
 
-  ngOnInit() {
+  async ngOnInit() {
     this.authService.appUser$
       .subscribe((appUser: AppUser) => {
         this.appUser = appUser;
       });
+
+    let cart$ = await this.cartService.getCart();
+    cart$.valueChanges().subscribe((cart: ShoppingCart) => {
+      let items: Item[] = Object.keys(cart.items)
+        .map(productKey => {
+          return cart.items[productKey];
+        });
+
+      this.quantity = 0;
+      items.forEach((item: Item) => {
+        this.quantity += item.quantity;
+      });
+
+    });
   }
 
   logout() {
