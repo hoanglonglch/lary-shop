f06faaabb868e95de3c8a77c9378af7eaf8b720c
Larry <larry@enclave.com>
2018-12-14 14:02:37 +0700

    [Checkout] Init UI

--- src/app/check-out/check-out.component.ts
+++ src/app/check-out/check-out.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-check-out',
   templateUrl: './check-out.component.html',
   styleUrls: ['./check-out.component.css']
 })
-export class CheckOutComponent implements OnInit {
+export class CheckOutComponent {
+  shipping = {};
+  shippingForm: FormGroup;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private fb: FormBuilder) {
+    this.shippingForm = this.initForm();
   }
 
+  placeOrder() {
+    console.log(this.shippingForm.value);
+  }
+
+  initForm() {
+    return this.fb.group({
+      name: ['', [
+        Validators.required
+      ]],
+      address: ['', [
+        Validators.required
+      ]],
+      city: ['', [
+        Validators.required
+      ]]
+    });
+  }
 }
