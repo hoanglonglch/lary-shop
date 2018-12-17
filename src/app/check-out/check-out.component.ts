4fb6433f1a79316f7cd31d5e5663c5f2f5197b85
Larry <larry@enclave.com>
2018-12-17 13:50:55 +0700

    [Checkout][order] Add userId to orders

--- src/app/check-out/check-out.component.ts
+++ src/app/check-out/check-out.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {CartService} from '../service/cart.service';
 import {ShoppingCart} from '../../models/shopping-cart';
 import {take} from 'rxjs/operators';
 import {OrderService} from '../order.service';
 import {ShoppingCartItem} from '../../models/shopping-cart-item';
+import {AuthService} from '../service/auth.service';
 
 @Component({
   selector: 'app-check-out',
@@ -15,18 +16,24 @@ export class CheckOutComponent implements OnInit {
   shipping = {};
   shippingForm: FormGroup;
   cart: ShoppingCart;
+  userId: string;
 
   constructor(private fb: FormBuilder,
               private cartService: CartService,
-              private orderService: OrderService) {
+              private orderService: OrderService,
+              private authService: AuthService) {
     this.shippingForm = this.initForm();
   }
 
-  async ngOnInit(){
+  async ngOnInit() {
     let cart$ = await this.cartService.getCart();
     cart$.valueChanges().pipe(take(1)).subscribe((cart: ShoppingCart) => {
       console.log('data', cart);
       this.cart = new ShoppingCart(cart.items);
+    });
+
+    this.authService.user$.pipe(take(1)).subscribe(data => {
+      this.userId = data.uid;
     });
   }
 
@@ -34,6 +41,7 @@ export class CheckOutComponent implements OnInit {
   placeOrder() {
     if (this.shippingForm.valid) {
       let order = {
+        userId: this.userId,
         dateCreated: new Date().getTime(),
         shipping: this.shippingForm.value,
         items: this.cart.listItems.map((item: ShoppingCartItem) => {
